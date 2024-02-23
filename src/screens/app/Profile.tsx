463600b5da22e39738e02017c024a4acc84cfa30
Guilherme Rera <guilhermerera@gmail.com>
2024-02-22 23:23:46 -0300

    feat: user photo selection implemented

--- src/screens/app/Profile.tsx
+++ src/screens/app/Profile.tsx
@@ -1,4 +1,5 @@
-import { Avatar } from "@components/Avatar";
+import { useState } from "react";
+import { Alert, TouchableOpacity } from "react-native";
 import { ScreenHeader } from "@components/ScreenHeader";
 import {
 	Center,
@@ -6,20 +7,75 @@ import {
 	ScrollView,
 	Skeleton,
 	Text,
-	VStack
+	VStack,
+	useToast
 } from "native-base";
-import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+
 import { Input } from "@components/Input";
+import { Avatar } from "@components/Avatar";
 import { Button } from "@components/Button";
 
 const AVATAR_SIZE = 32;
 
 export function Profile() {
-	const [isPhotoLoading, setIsPhotoLoading] = useState(true);
-	function handleSelectPhoto() {
-		alert("handle select photo");
+	const [isPhotoLoading, setIsPhotoLoading] = useState(false);
+	const [userPhoto, setUserPhoto] = useState(
+		"https://github.com/guilhermerera.png"
+	);
+
+	const toast = useToast();
+	async function handleSelectPhoto() {
+		setIsPhotoLoading(true);
+		try {
+			const { canceled, assets } = await ImagePicker.launchImageLibraryAsync({
+				mediaTypes: ImagePicker.MediaTypeOptions.Images,
+				allowsEditing: true,
+				aspect: [1, 1],
+				quality: 1,
+				allowsMultipleSelection: false
+			});
+
+			if (canceled) {
+				return;
+			}
+
+			if (assets[0].uri) {
+				const photoInfo = await FileSystem.getInfoAsync(assets[0].uri);
+				if (photoInfo.size && photoInfo.size / 1024 / 1024 > 1) {
+					toast.show({
+						title: "Imagem muito grande",
+						_title: {
+							color: "white",
+							fontSize: "md",
+							fontFamily: "heading",
+							textAlign: "center",
+							marginTop: 2
+						},
+						description: "Escolha uma de no máximo 1MB.",
+						_description: {
+							color: "white",
+							fontSize: "md",
+							fontFamily: "body",
+							marginX: 10,
+							marginBottom: 2
+						},
+						placement: "top",
+						duration: 3000,
+						bgColor: "red.500"
+					});
+					return;
+				}
+			}
+			setUserPhoto(assets[0].uri);
+		} catch (e) {
+			console.log(e);
+		} finally {
+			setIsPhotoLoading(false);
+		}
 	}
+
 	return (
 		<VStack flex={1}>
 			<ScreenHeader label='Perfil do Usuário' />
@@ -36,7 +92,7 @@ export function Profile() {
 					) : (
 						<Avatar
 							size={AVATAR_SIZE}
-							source={{ uri: "https://github.com/guilhermerera.png" }}
+							source={{ uri: userPhoto }}
 							alt='Avatar do usuário'
 						/>
 					)}
