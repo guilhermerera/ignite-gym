bc97683ffcbed27e90bc32c7a50a5b65dcae4de6
Guilherme Rera <guilhermerera@gmail.com>
2024-02-27 01:06:01 -0300

    add: user persistence with asyncstorage and loading state for ux

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { storageUserSave, storageUserGet } from "@storage/storageUser";
+
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
 
 export type AuthContentDataPropos = {
 	user: UserDTO;
 	logIn: (email: string, password: string) => Promise<void>;
+	isLoadingUserStorageDate: boolean;
 };
 
 type AuthContextProviderProps = {
@@ -17,6 +20,8 @@ export const AuthContext = createContext<AuthContentDataPropos>(
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
 	const [user, setUser] = useState<UserDTO>({} as UserDTO);
+	const [isLoadingUserStorageDate, setIsLoadingUserStorageDate] =
+		useState(true);
 
 	async function logIn(email: string, password: string) {
 		try {
@@ -27,14 +32,32 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
 			if (data.user) {
 				setUser(data.user);
+				await storageUserSave(data.user);
 			}
 		} catch (error) {
 			throw error;
 		}
 	}
 
+	async function loadUserData() {
+		try {
+			const user = await storageUserGet();
+			if (user) {
+				setUser(user);
+			}
+		} catch (error) {
+			throw error;
+		} finally {
+			setIsLoadingUserStorageDate(false);
+		}
+	}
+
+	useEffect(() => {
+		loadUserData();
+	}, []);
+
 	return (
-		<AuthContext.Provider value={{ user, logIn }}>
+		<AuthContext.Provider value={{ user, logIn, isLoadingUserStorageDate }}>
 			{children}
 		</AuthContext.Provider>
 	);
